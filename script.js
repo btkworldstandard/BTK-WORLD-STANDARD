d4235dd37c8a293987d671fb8fb9300b2558d6ea
btkworldstandard <btk.techn@gmail.com>
2026-07-27 21:01:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+const language = document.getElementById("language");
+
+language.addEventListener("change", function () {
+
+if (this.value === "English") {
+location.href = "index.html";
+}
+
+if (this.value === "Français") {
+location.href = "index-fr.html";
+}
+
+if (this.value === "العربية") {
+location.href = "index-ar.html";
+}
+
+if (this.value === "Español") {
+location.href = "index-es.html";
+}
+
+}); 
