4bbea6f4ef3cff01555dd3ee9202fe0f761051a8
btkworldstandard <btk.techn@gmail.com>
2026-07-27 21:16:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-
 const language = document.getElementById("language");
 
 language.addEventListener("change", function () {
 
-if (this.value === "English") {
-location.href = "index.html";
-}
+    if (this.value === "English") {
+        location.href = "index.html";
+    }
 
-if (this.value === "Français") {
-location.href = "index-fr.html";
-}
+    if (this.value === "Français") {
+        location.href = "index.html";
+    }
 
-if (this.value === "العربية") {
-location.href = "index-ar.html";
-}
+    if (this.value === "العربية") {
+        location.href = "index.html";
+    }
 
-if (this.value === "Español") {
-location.href = "index-es.html";
-}
+    if (this.value === "Español") {
+        location.href = "index.html";
+    }
 
 }); 
